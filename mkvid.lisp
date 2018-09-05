8e33dff62834eed04ad710a3a2962f0878311791
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-09-06 00:00:47 +0800

    Split out the files into portions for easier organisation.

--- mkvid.lisp
+++ mkvid.lisp
@@ -3,25 +3,6 @@
 (in-package #:mkvid)
 (in-readtable :qtools)
 
-(define-widget main-window (QWidget)
-  ((stage-tl-corner-x :initform 60
-                      :initarg :stage-topleft-x
-                      :accessor stage-topleft-x)
-   (stage-tl-corner-y :initform 60
-                      :initarg :stage-topleft-y
-                      :accessor stage-topleft-y)
-   (stage-width :initform 1280
-                :initarg :stage-width
-                :accessor stage-width)
-   (stage-height :initform 720
-                 :initarg :stage-height
-                 :accessor stage-height)))
-
-(defparameter *background-colour* (q+:make-qcolor 0 10 25)
-  "The background colour for the stage.")
-(defparameter *text-colour* (q+:make-qcolor 240 240 15)
-  "The foreground colour for the stage.")
-
 (defgeneric absolute-stage-coordinates (window dimension offset)
   (:documentation "Calculate the coordinates of a particular point in a stage,
 with the offset given as the number of absolute pixels. ")
@@ -80,38 +61,6 @@ with the provided width and height, then shift it by some amount.")
                     (relative-stage-coordinates* window :x dx)
                     (relative-stage-coordinates* window :y dy))
       box)))
-
-;; (define-subwidget (viewer timer) (q+:make-qtimer viewer)
-;;   (setf (q+:single-shot timer) nil)
-;;   (q+:start timer (round 1000/60)))
-
-;; (defclass style-combination ()
-;;   ((font-face :initarg :font-face
-;;               :accessor font-face
-;;               :initform nil)
-;;    (font-size :initarg :font-size
-;;               :accessor font-size
-;;               :initform nil)
-;;    (fg-colour :initarg :fg-colour
-;;               :accessor fg-colour
-;;               :initform nil)
-;;    (bg-colour :initarg :bg-colour
-;;               :accessor bg-colour
-;;               :initform nil))
-;;   (:documentation "A combination of styles."))
-
-;; (defgeneric make-font (style-combination)
-;;   (:documentation "Make a font object based on the style combination.")
-;;   (:method ((style-combination style-combination))
-;;     (q+:make-qfont (font-face style-combination)
-;;                    (font-size style-combination))))
-
-;; (defgeneric )
-
-;; (define-slot (viewer update) ()
-;;   (declare (connected timer (timeout)))
-;;   (update scene)
-;;   (q+:repaint viewer))
 
 (defmacro with-brush-pen-font ((painter brush pen font) &body body)
   (alexandria:with-gensyms (old-brush old-pen old-font)
---
+++ window.lisp
@@ -0,0 +1,53 @@
+;;;; window.lisp
+
+(in-package #:mkvid)
+(in-readtable :qtools)
+
+(define-widget main-window (QWidget)
+  ((stage-tl-corner-x :initform 60
+                      :initarg :stage-topleft-x
+                      :accessor stage-topleft-x)
+   (stage-tl-corner-y :initform 60
+                      :initarg :stage-topleft-y
+                      :accessor stage-topleft-y)
+   (stage-width :initform 1280
+                :initarg :stage-width
+                :accessor stage-width)
+   (stage-height :initform 720
+                 :initarg :stage-height
+                 :accessor stage-height)))
+
+(defparameter *background-colour* (q+:make-qcolor 0 10 25)
+  "The background colour for the stage.")
+(defparameter *text-colour* (q+:make-qcolor 240 240 15)
+  "The foreground colour for the stage.")
+
+;; (define-subwidget (viewer timer) (q+:make-qtimer viewer)
+;;   (setf (q+:single-shot timer) nil)
+;;   (q+:start timer (round 1000/60)))
+
+;; (defclass style-combination ()
+;;   ((font-face :initarg :font-face
+;;               :accessor font-face
+;;               :initform nil)
+;;    (font-size :initarg :font-size
+;;               :accessor font-size
+;;               :initform nil)
+;;    (fg-colour :initarg :fg-colour
+;;               :accessor fg-colour
+;;               :initform nil)
+;;    (bg-colour :initarg :bg-colour
+;;               :accessor bg-colour
+;;               :initform nil))
+;;   (:documentation "A combination of styles."))
+
+;; (defgeneric make-font (style-combination)
+;;   (:documentation "Make a font object based on the style combination.")
+;;   (:method ((style-combination style-combination))
+;;     (q+:make-qfont (font-face style-combination)
+;;                    (font-size style-combination))))
+
+;; (define-slot (viewer update) ()
+;;   (declare (connected timer (timeout)))
+;;   (update scene)
+;;   (q+:repaint viewer))
