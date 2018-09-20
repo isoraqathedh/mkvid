621842f7439f7bc456e6fefb6e529e2ef4c4fdd7
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-09-20 12:49:48 +0800

    Import the things from Flare.

--- package.lisp
+++ package.lisp
@@ -3,4 +3,5 @@
 (defpackage #:info.isoraqathedh.mkvid
   (:nicknames #:mkvid)
   (:use #:cl+qt)
+  (:import-from #:flare #:particle #:scene #:define-progression #:enter #:start #:calc)
   (:export #:main))
