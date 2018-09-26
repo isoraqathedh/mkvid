68566fa8824ebee8f69923b26bd0ba87cd9aadd3
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-09-26 20:13:21 +0800

    Create and implement a clock.

--- window.lisp
+++ window.lisp
@@ -3,8 +3,16 @@
 (in-package #:mkvid)
 (in-readtable :qtools)
 
+(defparameter *default-fps* 60
+  "The rate at which the animation should be run at.")
+
 (define-widget main-window (QWidget)
-  ())
+  ((counter :accessor counter
+            :initform 0
+            :documentation "A counter for a particular timer.")
+   (fps :accessor fps
+        :initform *default-fps*
+        :initarg :fps)))
 
 ;;; Stage
 ;;; a stage is a place where all the action is held and recorded.
@@ -26,22 +34,33 @@
 
 (define-subwidget (main-window the-stage) (make-instance 'qstage))
 
-(define-subwidget (main-window visible-test) (q+:make-qlcdnumber main-window)
-  (q+:set-digit-count visible-test 5))
+;;; Clocks
+;;; This is the animation clock that motions would be timed against.
+;;; It also includes a display.
+(define-subwidget (main-window seconds-display) (q+:make-qlcdnumber main-window)
+  (q+:set-digit-count seconds-display 10)
+  (q+:display seconds-display "--"))
+(define-subwidget (main-window seconds-label) (q+:make-qlabel "Seconds - Frames"))
 
-(define-subwidget (main-window layout) (q+:make-qvboxlayout main-window)
-  (q+:add-widget layout the-stage)
-  (q+:add-widget layout visible-test))
+(define-subwidget (main-window clock) (q+:make-qgridlayout main-window)
+  (q+:add-widget clock seconds-label 0 0)
+  (q+:add-widget clock seconds-display 1 0 3 1))
 
+(define-subwidget (main-window clock-group-box) (q+:make-qgroupbox "Clock" main-window)
+  (q+:set-layout clock-group-box clock))
 
 (define-subwidget (main-window timer) (q+:make-qtimer main-window)
   (setf (q+:single-shot timer) nil)
-  (q+:start timer (round 1000/60)))
+  (q+:start timer (round (/ 1000 fps))))
 
-;; (define-slot (viewer update) ()
-;;   (declare (connected timer (timeout)))
-;;   (update scene)
-;;   (q+:repaint viewer))
+(define-slot (main-window update) ()
+  (declare (connected timer (timeout)))
+  (multiple-value-bind (seconds frames) (floor (incf counter) fps)
+    (q+:display seconds-display (format nil "~d-~2,'0d" seconds frames))))
+
+(define-subwidget (main-window layout) (q+:make-qvboxlayout main-window)
+  (q+:add-widget layout the-stage)
+  (q+:add-widget layout clock-group-box))
 
 (defun main ()
   (with-main-window (w 'main-window)
