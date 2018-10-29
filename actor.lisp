dd51a7b55c4d336ce0bdf93a03b9677b84052fb0
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-10-29 23:11:42 +0800

    New type of actor (test)

--- actor.lisp
+++ actor.lisp
@@ -142,3 +142,51 @@
 ;;; Text + rectangle
 (defclass textbox (box text)
   ())
+
+;;; Image actor
+(defclass image-actor (box)
+  ((image :initarg :qpixmap
+          :accessor qpixmap-of)
+   (sprite-location :initarg :sprite-location
+                    :accessor sprite-location))
+  (:default-initargs
+   :sprite-location (vec 0 0)))
+
+(defmethod initialize-instance :after ((object image-actor) &key image-file)
+  ;; Load the image.
+  (setf (qpixmap-of object)
+        (q+:make-qpixmap (namestring image-file))))
+
+(defmethod paint progn ((image-actor image-actor) painter)
+  (let ((sprite-width (vx (size image-actor)))
+        (sprite-height (vy (size image-actor))))
+    (q+:draw-pixmap
+     painter
+     0 0                                      ; location
+     sprite-width sprite-height               ; drawing rectangle size
+     (qpixmap-of image-actor)                 ; image
+     ;; The location of the sprite
+     ;; We'll take the size of the sprite as the size of the actor,
+     ;; and then take the vxth column of the vyth row
+     ;; of the image divided into rectangles of size that.
+     (floor (* (vx (sprite-location image-actor))
+               (vx (size image-actor))))
+     (floor (* (vy (sprite-location image-actor))
+               (vy (size image-actor))))
+     sprite-width sprite-height         ; sprite rectangle size
+     )))
+
+(defmethod flare:leave :after ((unit image-actor) scene-graph)
+  (declare (ignore scene-graph))
+  ;; Need to clean up the qpixmap after the actor leaves
+  ;; as it's basically impossible to access the actor after leaving
+  ;; we'll declare it dead and just finalize the thing.
+  (finalize (qpixmap-of unit)))
+
+;;; Stepper actor
+;;; a stepper actor has a number of "masks" (steps)
+;;; that can be stepped through.
+
+;;; Blinkenlights actor
+;;; a single actor that controls an array of lights;
+;;; can be used to switch on or off a list of available things.
