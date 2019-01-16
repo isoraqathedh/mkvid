257bd789d78c5f6dc151a07aa17e8236337eb920
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2019-01-16 17:35:44 +0800

    Start writing stuff for last-this-next actors.

--- actor.lisp
+++ actor.lisp
@@ -220,6 +220,32 @@
             (nconc (locations shifter-actor)
                    (list top))))))
 
+(defmethod flare:ease-object ((from list) (to list) x by)
+  (let ((len (length from)))
+    (mapcar #'flare:ease-object from to
+            (make-list len :initial-element x)
+            (make-list len :initial-element by))))
+
+;;; Last/this/next actor
+;;; A special form of shifter actor that has three positions:
+;;; a specific "this" position and the associated actor,
+;;; with everything that comes before it being in "last" positions
+;;; and everything that comes after it is in the "future" positions.
+;;; Each position has a specific style that will stay on the positions,
+;;; not the object (i.e. fixed values for some slots.)
+;;;
+;;; It is required that all the actors have to have the same slots,
+;;; as it makes no sense to convert one actor type to another.
+
+(defclass last/this/next-actor (group-actor)
+  ((this-position :accessor this-position)))
+
+(defgeneric tween-to-last (group-actor subactor))
+(defgeneric tween-to-this (group-actor subactor))
+(defgeneric tween-to-next (group-actor subactor))
+
+;; (defmethod flare:enter ())
+
 ;;; Indicator bar actor
 (defclass abstract-bar-actor (box)
   ((bar-maximum :initarg :maximum :accessor bar-maximum)
