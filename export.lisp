4b082815a31d58f361f26d34b9efd0262a2c0bc2
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2019-04-05 16:46:48 +0800

    More tentative steps toward image export.

--- export.lisp
+++ export.lisp
@@ -8,3 +8,21 @@
    #p"frames/"
    (asdf:system-relative-pathname 'mkvid nil))
   "Output folder for frames.")
+
+(define-slot (canvas dump-image) ()
+  (with-finalizing* ((canvas-rect (q+:rect canvas))
+                     (region (q+:make-qregion canvas-rect))
+                     (origin (q+:make-qpoint))
+                     (img (q+:make-qpixmap
+                           (q+:width size)
+                           (q+:height size))))
+    ;; widget->render(&pixmap, QPoint(), QRegion(rectangle));
+    (q+:render canvas img origin region)
+    (q+:save img (namestring (merge-pathnames
+                              (make-pathname :name "test" :type "png")
+                              *frames-folder*)))))
+
+(defun frames->video (dir)
+  "Create a video with the frames in DIR.")
+
+;;; To do: bypass the filesystem and find some way to use `clave' directly.
