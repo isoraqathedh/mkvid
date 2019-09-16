29a0ab1138dd744bf289045f6800fe7bf4fee5d9
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2019-09-16 22:31:26 +0800

    Add a test suite for the new actor tests.

---
+++ actor-tests.lisp
@@ -0,0 +1,20 @@
+;;;; actor-tests.lisp
+(in-package #:mkvid)
+(in-readtable :qtools)
+
+(defclass actor-test-textbox (aux-data-textbox)
+  ()
+  (:default-initargs
+   :font-size 16
+   :size (vec 250 60)
+   :background *background-colour*))
+
+(define-presentation group-actor-test (1024 576)
+  1 (t (enter group-actor :name :group :location (vec 400 0)))
+  2 (:group (enter actor-test-textbox :location (vec 100 100)
+                                      :text "[A]"
+                                      :name :text-A))
+  3 (:group (enter actor-test-textbox :location (vec 300 300)
+                                      :text "[B]"
+                                      :name :text-B))
+  4 4.5 (:group (set location :to (vec 250 0) :ease flare:quad-out)))
